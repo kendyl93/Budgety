12704636182ad83eb4eda08a3413a77e5b53dc70
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-09-29 21:51:05 +0200

    Added passport and check if callback works - its ok -- neet to save/update user and redirect

--- sign-in-server/environment.js
+++ sign-in-server/environment.js
@@ -1,6 +1,29 @@
 require('dotenv/config');
 
-export const { SIGN_IN_PORT } = process.env;
+const {
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  FACEBOOK_CALLBACK,
+  HOST_URI,
+  REVERSE_PROXY_PORT,
+  SIGN_IN_PORT
+} = process.env;
+
+const SIGN_IN_FACEBOOK_CALLBACK = `http://${HOST_URI}:${REVERSE_PROXY_PORT}/sign-in/${FACEBOOK_CALLBACK}`;
+
+export {
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  FACEBOOK_CALLBACK,
+  SIGN_IN_PORT,
+  SIGN_IN_FACEBOOK_CALLBACK
+};
 
 console.log('SIGN-IN SERVER:');
-console.log({ SIGN_IN_PORT });
+console.log({
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  FACEBOOK_CALLBACK,
+  SIGN_IN_PORT,
+  SIGN_IN_FACEBOOK_CALLBACK
+});
--- sign-in-server/index.js
+++ sign-in-server/index.js
@@ -1,16 +1,51 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
 import cors from 'cors';
-import { SIGN_IN_PORT } from './environment';
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import {
+  FACEBOOK_CALLBACK,
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  SIGN_IN_PORT,
+  SIGN_IN_FACEBOOK_CALLBACK
+} from './environment';
+
+console.log('SIGN IN server');
+console.log({
+  FACEBOOK_CALLBACK,
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  SIGN_IN_PORT,
+  SIGN_IN_FACEBOOK_CALLBACK
+});
 
 const app = express();
 app.use(cors());
-// app.use(bodyParser.json());
 
-// db_connect();
-app.get('/sign-in', (req, res) => {
-  res.send('Sign in server - welcome');
-});
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: SIGN_IN_FACEBOOK_CALLBACK
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOrCreate({ facebookId: profile.id }, (err, user) => {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+app.get('/sign-in', passport.authenticate('facebook'));
+
+app.get(
+  `/sign-in/${FACEBOOK_CALLBACK}`,
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/failure'
+  })
+);
 
 app.listen(SIGN_IN_PORT, () => {
   console.log('Sign in server is running on Port: ' + SIGN_IN_PORT);
