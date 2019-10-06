fa0d0a187cf2b008c64f0acd29a8088288e7fec3
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-10-06 19:19:14 +0200

    Added sign-in component in sign-in-server

---
+++ sign-in-server/src/sign-in.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn = () => (
+  <div>
+    <h1>Sign in</h1>
+    <h5>In order to use the app, please sign in.</h5>
+
+    <a href="/sign-in">sign in</a>
+  </div>
+);
+
+export default SignIn;
