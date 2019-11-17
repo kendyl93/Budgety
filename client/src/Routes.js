5746f2e939c7be4b686a4072428fb4003c333931
Pawel Stanecki <p.stanecki93@gmail.com>
2019-11-17 19:05:01 +0100

    Added login page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,12 +5,9 @@ import Todos from './todos/Todos';
 import Create from './todos/Create';
 import Edit from './todos/Edit';
 
-import Login from './Login';
-
 const Routes = () => (
   <Switch>
     <Route component={Todos} exact path="/" />
-    <Route component={Login} path="/login" />
     <Route component={Edit} path="/edit/:id" />
     <Route component={Create} path="/create" />
   </Switch>
---
+++ client/src/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleButton from './GoogleButton';
+
+const Login = () => (
+  <div className="login-wrapper">
+    <h1>Budgety</h1>
+    <p>In order to use the app, please log in.</p>
+    <GoogleButton />
+  </div>
+);
+
+export default Login;
