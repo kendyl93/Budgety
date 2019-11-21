5e150d2e5dc047339c07602d1acd442da09041f8
Pawel Stanecki <p.stanecki93@gmail.com>
2019-11-21 19:44:22 +0100

    Removed dummy app title

--- client/src/Main.js
+++ client/src/Main.js
@@ -4,13 +4,6 @@ import Routes from './Routes';
 
 const Main = () => (
   <div className="container">
-    <h1>
-      Budgety app <br />
-      <small>
-        This app is created to autmate my hommies budget calculations.
-      </small>
-    </h1>
-
     <Routes />
   </div>
 );
